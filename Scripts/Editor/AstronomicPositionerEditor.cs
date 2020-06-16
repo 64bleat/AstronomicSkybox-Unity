ddcc2c5c851e4264db20b7c281b360b032e9594d
Shrike <48031028+64bleat@users.noreply.github.com>
2020-06-15 22:16:03 -0700

    Scripts

---
+++ Scripts/Editor/AstronomicPositionerEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace SHK.Astronomy
+{
+    [CustomEditor(typeof(AstronomicPositioner))]
+    public class AstronomicPositionerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("GenerateStars"))
+            {
+                AstronomicPositioner t = (AstronomicPositioner)target;
+                t.GenerateStarMeshes();
+            }
+        }
+    }
+}
